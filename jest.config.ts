48594205274ac27fc622711b0e347b94befb0249
afolarte <andy.olarte514@gmail.com>
2026-06-02 13:40:03 -0500

    fix: correct jest setupFilesAfterEnv + add @testing-library/dom

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({ dir: "./" });
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  setupFilesAfterEachTest: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["**/__tests__/**/*.test.{ts,tsx}", "**/*.test.{ts,tsx}"],
   testPathIgnorePatterns: ["<rootDir>/e2e/"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/**/*.stories.{ts,tsx}"],
